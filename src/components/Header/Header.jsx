b30577011608b7f22987ff5161da2124da6641b8
Victor Lisboa <jvclisboa@outlook.com>
2021-08-12 13:57:36 -0300

    add sass variables and dark/light theme switcher

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,28 @@
+
 import React from 'react'
 import { HiOutlineMoon } from "react-icons/hi";
 import './Header.scss'
 
+
 const Header = () => {
+    
+    const changeTheme = () => {
+        if(!localStorage.getItem('dark')) {
+            localStorage.setItem('dark', 'dark-theme')
+            document.body.classList.add('dark')
+        } else {
+            localStorage.removeItem('dark')
+            document.body.classList.remove('dark')
+        }
+    }
+
     return (
         <div>
             <header className="header">
                 <div className="header-child">
                     <h3>Where in the world?</h3>
                 </div>
-                <div className="header-child">
+                <div className="header-child" onClick={changeTheme}>
                     <span id="moon-icon"><HiOutlineMoon /></span> 
                     <p>Dark Mode</p>
                 </div>
